3d6d5607ca388ecbebb252cdd00854fc9616e24e
y-emam <YasserEmam2002@gmail.com>
2023-10-28 14:52:50 +0200

    added the basic UI for song Details

--- src/pages/songDetails.jsx
+++ src/pages/songDetails.jsx
@@ -1,18 +1,37 @@
 import { useSelector } from "react-redux";
 import Navbar from "../components/navbar";
+import "./songDetails.css";
+import { FaClock } from "react-icons/fa";
 
 function SongDetails() {
   const song = useSelector((state) => state.playList.value.currentPlayingSong);
 
+  const packages = [1, 2, 3, 4];
+
+  const packagesComponents = packages.map((songPackage) => {
+    return <div>some shit</div>;
+  });
   return (
     <div className="song-details">
       <Navbar />
       {song._id ? (
-        <div className="header">
-          <img src={song.imageUrl} alt="song-img" />
-          <div className="content">
-            {song.name} <div className="tags"></div>
+        <div>
+          <div className="header">
+            <img src={song.imageUrl} alt="song-img" />
+            <div className="content">
+              <p className="name">{song.name}</p>
+              <div style={{ display: "flex" }}>
+                <p className="bpm">BPM: {song.bpm}</p>
+                &nbsp; &nbsp;
+                <p className="duration">
+                  <FaClock /> {song.duration}
+                </p>
+              </div>
+              <p className="date">{song.date}</p>
+              <div className="tags"></div>
+            </div>
           </div>
+          <div className="packages">{packagesComponents}</div>
         </div>
       ) : (
         "Loading..."
